4212f79de2f8541be3bf9e6abbb06acc2c4febc3
Akshaj <akshaj@Akshajs-MacBook-Air.local>
2025-06-04 18:07:25 +0530

    added middleware multer and cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/temp"); // Specify the directory to store uploaded files
+  },
+  filename: (req, file, cb) => {    
+    cb(null, file.originalname);
+  }
+});
+
+export const upload = multer({
+  storage,
+})
